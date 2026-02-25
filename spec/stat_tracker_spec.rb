66688ce6d17dae7a725dbd9b2d8436246204a13b
shawndcarpenter <shawncarpenter.co@gmail.com>
2026-02-25 12:39:24 -0600

    add tests for most and least accurate team

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -204,12 +204,14 @@ RSpec.describe StatTracker do
     describe '#most_accurate_team' do
     	it 'returns Name of the Team with the best ratio of shots to goals for the season as a String' do
         expect(@stats.most_accurate_team("20122013")).to be_a_instance_of String
+        expect(@stats.most_accurate_team("20132014")).to eq("New York Red Bulls").or(eq("FC Dallas"))
     	end
     end
 
-    skip '#least_accurate_team' do
+    describe '#least_accurate_team' do
     	it 'returns Name of the Team with the worst ratio of shots to goals for the season as a	String' do
         expect(@stats.least_accurate_team("20122013")).to be_a_instance_of String
+        expect(@stats.least_accurate_team("20132014")).to eq("New York Red Bulls").or(eq("FC Dallas"))
     	end
 
     end 
